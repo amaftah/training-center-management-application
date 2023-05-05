a1db61bfdd807380414292caac5e84586cc68266
Abderrahmane Maftah <78481502+mafghal@users.noreply.github.com>
2023-05-05 21:31:22 +0000

    t

--- db_connection.php
+++ db_connection.php
@@ -1,6 +1,6 @@
 <?php 
             try{
-              $db_connection = new PDO("mysql:host=127.0.0.1;dbname=centre;charset=utf8mb4;", 'root', '');
+              $db_connection = new PDO("mysql:host=https://mafghal-zany-fortnight-p6w75996pxxc6vvg-8081.preview.app.github.dev:8081;dbname=centre;charset=utf8mb4;", 'root', 'rootpass');
             }
             catch(PDOException $e){
               echo 'Erreur : ' . $e->getMessage();
